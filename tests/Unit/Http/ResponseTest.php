d2de648adda0e2379c5835081219ed7eae6737e9
Ashley Gibson <ashley@nosegraze.com>
2022-04-09 16:09:21 +0100

    Response tests.

---
+++ tests/Unit/Http/ResponseTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * ResponseTest.php
+ *
+ * @package   contextwp-sdk
+ * @copyright Copyright (c) 2022, Ashley Gibson
+ * @license   GPL2+
+ */
+
+namespace ContextWP\Tests\Unit\Http;
+
+use ContextWP\Http\Response;
+use ContextWP\Tests\TestCase;
+
+class ResponseTest extends TestCase
+{
+    /**
+     * @covers \ContextWP\Http\Response::__construct()
+     */
+    public function testCanConstruct(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \ContextWP\Http\Response::makeFromWpError()
+     */
+    public function testCanMakeFromWpError(): void
+    {
+        $error = \Mockery::mock('WP_Error');
+        $error->expects('get_error_message')
+            ->once()
+            ->andReturn('error message');
+
+        $response = Response::makeFromWpError($error);
+
+        $this->assertSame(503, $response->responseCode);
+        $this->assertSame('error message', $response->responseBody);
+    }
+
+    /**
+     * @covers       \ContextWP\Http\Response::isOk()
+     * @dataProvider providerIsOk
+     */
+    public function testIsOk(int $responseCode, bool $expected): void
+    {
+        $response = new Response($responseCode);
+
+        $this->assertSame($expected, $response->isOk());
+    }
+
+    public function providerIsOk(): \Generator
+    {
+        yield '200 is ok' => [200, true];
+        yield '201 is ok' => [201, true];
+        yield '204 is ok' => [204, true];
+        yield '301 not ok' => [301, false];
+        yield '422 not ok' => [422, false];
+        yield '500 not ok' => [500, false];
+        yield '503 not ok' => [503, false];
+    }
+}
