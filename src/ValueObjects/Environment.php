e2e81ef69f536eb740404956150d2a6f948581e4
Ashley Gibson <ashley@nosegraze.com>
2022-04-09 14:57:01 +0100

    Source hasher.

--- src/ValueObjects/Environment.php
+++ src/ValueObjects/Environment.php
@@ -10,19 +10,24 @@
 namespace ContextWP\ValueObjects;
 
 use ContextWP\Contracts\Arrayable;
+use ContextWP\Helpers\SourceHasher;
 use ContextWP\SDK;
 
 class Environment implements Arrayable
 {
-
     public function toArray(): array
     {
         return [
-            'source_hash' => '',
+            'source_hash' => $this->getSourceHash(),
             'wp_version'  => '',
             'php_version' => '',
             'locale'      => '',
             'sdk_version' => SDK::getVersion(),
         ];
+    }
+
+    protected function getSourceHash(): string
+    {
+        return (new SourceHasher())->getHash();
     }
 }
