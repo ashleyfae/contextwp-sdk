3664950f2c5b27f5e3d8ee57bbe4a85b94162d43
Ashley Gibson <ashley@nosegraze.com>
2022-04-14 16:57:34 +0100

    Base implementation.

--- src/Cli/Commands/SendCheckInsCommand.php
+++ src/Cli/Commands/SendCheckInsCommand.php
@@ -9,10 +9,16 @@
 
 namespace ContextWP\Cli\Commands;
 
+use ContextWP\Actions\SendCheckIns;
 use ContextWP\Contracts\CliCommand;
+use Exception;
+use WP_CLI;
 
 class SendCheckInsCommand implements CliCommand
 {
+    /** @var SendCheckIns $sendCheckIns */
+    protected $sendCheckIns;
+
     /**
      * @inheritDoc
      */
@@ -23,6 +29,10 @@ class SendCheckInsCommand implements CliCommand
 
     public function __invoke(array $args, array $assocArgs): void
     {
-        // TODO: Implement __invoke() method.
+        try {
+            $this->sendCheckIns->execute();
+        } catch (Exception $e) {
+            WP_CLI::error(sprintf('%s: %s', get_class($e), $e->getMessage()));
+        }
     }
 }
