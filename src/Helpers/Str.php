2a18b720020e421e5f0c5a0ba19b04f76f6d830d
Ashley Gibson <ashley@nosegraze.com>
2022-04-15 08:44:38 +0100

    Tests & more string helpers.

--- src/Helpers/Str.php
+++ src/Helpers/Str.php
@@ -14,6 +14,8 @@ class Str
     /**
      * Sanitizes a string.
      *
+     * @since 1.0
+     *
      * @param  mixed  $string
      *
      * @return string
@@ -25,8 +27,30 @@ class Str
         return function_exists('sanitize_text_field') ? sanitize_text_field($string) : $string;
     }
 
+    /**
+     * Determines if the provided string is a UUID.
+     *
+     * @since 1.0
+     *
+     * @param  string  $string
+     *
+     * @return bool
+     */
     public static function isUuid(string $string): bool
     {
         return preg_match('/^[a-f\d]{8}(-[a-f\d]{4}){4}[a-f\d]{8}$/i', $string) === 1;
+    }
+
+    /**
+     * Cuts a string to the supplied number of characters, if the string is longer than the max characters.
+     *
+     * @param  string  $string
+     * @param  int  $maxLength
+     *
+     * @return string
+     */
+    public static function maxChars(string $string, int $maxLength): string
+    {
+        return substr($string, 0, $maxLength);
     }
 }
