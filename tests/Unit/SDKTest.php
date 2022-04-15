6c6649c5aa3e84f96b468e2a9053ae08b1c877f2
Ashley Gibson <ashley@nosegraze.com>
2022-04-15 09:20:14 +0100

    Exception is not thrown.

--- tests/Unit/SDKTest.php
+++ tests/Unit/SDKTest.php
@@ -50,7 +50,6 @@ class SDKTest extends TestCase
 
     /**
      * @covers \ContextWP\SDK::register()
-     * @throws ReflectionException
      */
     public function testCanRegister(): void
     {
